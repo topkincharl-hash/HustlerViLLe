08f2e36b961df296389e00f1c785db9e263b542f
topkincharl-hash <topkincharl@gmail.com>
2026-06-03 20:27:39 +0200

    Create Plugins/Serialization.kt

---
+++ feature-remote/src/main/kotlin/com/aiope2/feature/Plugins/Serialization.kt
@@ -0,0 +1,17 @@
+package com.aiope.daemon.plugins
+
+import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
+
+fun Application.configureSerialization() {
+    install(ContentNegotiation) {
+        json(
+            Json {
+                ignoreUnknownKeys = true
+                prettyPrint = false
+            }
+        )
+    }
+}
